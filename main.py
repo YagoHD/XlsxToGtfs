5267a7ad5bdc1a67a8f1ca0d4d098559c3b4dbbb
YagoHD <yagorb@hotmail.com>
2023-05-02 10:12:24 +0200

    ADD: Añadida interfaz. Se añadio una interfaz, que esta aun en desarrollo, para la comodidad del usuario.

--- main.py
+++ main.py
@@ -1,31 +1,66 @@
+import tkinter as tk
+from tkinter import filedialog
 import pandas as pd
 import numpy as np
 import os
 
-# Crea la carpeta "text_files" si no existe
-if not os.path.exists('../text_files'):
-    os.makedirs('../text_files')
+class ExcelToTextConverter:
+    def __init__(self, master):
+        self.master = master
+        master.title("Convertidor Xlsx -> Gtfs")
 
-# Lee el archivo Excel
-xl = pd.ExcelFile('../Informacion.xlsx')
+        # Crear los widgets
+        self.file_label = tk.Label(master, text="Seleccionar archivo de Excel:")
+        self.file_button = tk.Button(master, text="Buscar archivo", command=self.select_file)
+        self.filename_label = tk.Label(master, text="")
+        self.convert_button = tk.Button(master, text="Convertir a archivos de texto", state=tk.DISABLED, command=self.convert)
+        self.status_label = tk.Label(master, text="", fg="green")
 
-# Itera sobre todas las hojas del archivo de Excel
-for sheet_name in xl.sheet_names:
-    
-    # Lee los datos de la hoja del Excel
-    df = xl.parse(sheet_name)
-    
-    # Reemplaza los valores NaN por un espacio vacío
-    df.replace(np.nan, '', inplace=True)
+        # Colocar los widgets en la ventana
+        self.file_label.grid(row=0, column=0, padx=10, pady=10)
+        self.file_button.grid(row=0, column=1, padx=10, pady=10)
+        self.filename_label.grid(row=1, column=0, padx=10, pady=10)
+        self.convert_button.grid(row=1, column=1, padx=10, pady=10)
+        self.status_label.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
 
-    # Extrae la primera fila como encabezados de las columnas
-    headers = list(df.columns.values)
+    def select_file(self):
+        # Mostrar un diálogo para seleccionar el archivo de Excel
+        self.filename = filedialog.askopenfilename()
+        self.filename_label.config(text=self.filename)
+        self.convert_button.config(state=tk.NORMAL)
+        self.status_label.config(text="")
 
-    # Escribe los encabezados en el archivo de texto
-    with open(f'../text_files/{sheet_name}', 'w') as f:
-        f.write(','.join(headers) + '\n')
+    def convert(self):
+        # Lee el archivo Excel
+        xl = pd.ExcelFile(self.filename)
 
-    # Escribe los datos en el archivo de texto
-    with open(f'../text_files/{sheet_name}', 'a') as f:
-        for index, row in df.iterrows():
-            f.write(','.join([str(x) if str(x) != 'nan' else '' for x in row.tolist()]) + '\n')
+        # Crea la carpeta "text_files" si no existe
+        if not os.path.exists('../text_files'):
+            os.makedirs('../text_files')
+
+        # Itera sobre todas las hojas del archivo de Excel
+        for sheet_name in xl.sheet_names:
+
+            # Lee los datos de la hoja del Excel
+            df = xl.parse(sheet_name)
+
+            # Reemplaza los valores NaN por un espacio vacío
+            df.replace(np.nan, '', inplace=True)
+
+            # Extrae la primera fila como encabezados de las columnas
+            headers = list(df.columns.values)
+
+            # Escribe los encabezados en el archivo de texto
+            with open(f'../text_files/{sheet_name}', 'w') as f:
+                f.write(','.join(headers) + '\n')
+
+            # Escribe los datos en el archivo de texto
+            with open(f'../text_files/{sheet_name}', 'a') as f:
+                for index, row in df.iterrows():
+                    f.write(','.join([str(x) if str(x) != 'nan' else '' for x in row.tolist()]) + '\n')
+
+        self.status_label.config(text="La conversión se completó exitosamente.")
+
+root = tk.Tk()
+app = ExcelToTextConverter(root)
+root.mainloop()
--- setup.py
+++ setup.py
@@ -1,20 +1,33 @@
 import sys
+import os
 from cx_Freeze import setup, Executable
 
+# Obtener la ruta del directorio raíz del proyecto
+root_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Incluye los archivos necesarios en el ejecutable
+includefiles = ['text_files/']
+
+# Opciones para el ejecutable
 build_exe_options = {
     'packages': ['os', 'pandas', 'numpy'],
     'include_files': ['Informacion.xlsx','icono/icono.ico'],
     'include_msvcr': True,
     'build_exe': './dist', # Ruta donde se creará el ejecutable
-    'excludes': ['tkinter']
 }
 
+# Configuración del ejecutable
 base = None
-if sys.platform == "win32":
-    base = "Win32GUI"
+if sys.platform == 'win32':
+    base = 'Win32GUI'
 
+executables = [
+    Executable('main.py', base=base, target_name='ExcelToTextConverter')
+]
+
+# Configuración del setup
 setup(name="mi_app",
       version="1.0",
-      description="Descripción de mi app",
+      description='Convierte un archivo Excel a archivos de texto',
       options={"build_exe": build_exe_options},
       executables=[Executable("main.py", base=base, target_name="XlsxToGtfs.exe",icon='icono/icono.ico')])
