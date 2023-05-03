2ebca85dcfd0579072a3709cf53cf5a3d4a59646
YagoHD <yagorb@hotmail.com>
2023-05-03 08:55:34 +0200

    MOD: Orden y optimizacion

--- codigo/setup.py
+++ codigo/setup.py
@@ -5,15 +5,12 @@ from cx_Freeze import setup, Executable
 # Obtener la ruta del directorio raíz del proyecto
 root_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Incluye los archivos necesarios en el ejecutable
-includefiles = ['text_files/']
-
-# Opciones para el ejecutable
+# Opciones para el ejecutable: paquetes incluidos, archivos incluidos & la ruta donde se creará el freeze
 build_exe_options = {
     'packages': ['os', 'pandas', 'numpy'],
-    'include_files': ['Informacion.xlsx','icono/icono.ico'],
+    'include_files': ['Informacion.xlsx','codigo/icono/icono.ico'],
     'include_msvcr': True,
-    'build_exe': './dist', # Ruta donde se creará el ejecutable
+    'build_exe': './dist', 
 }
 
 # Configuración del ejecutable
@@ -30,4 +27,4 @@ setup(name="mi_app",
       version="1.0",
       description='Convierte un archivo Excel a archivos de texto',
       options={"build_exe": build_exe_options},
-      executables=[Executable("main.py", base=base, target_name="XlsxToGtfs.exe",icon='icono/icono.ico')])
+      executables=[Executable("codigo/main.py", base=base, target_name="XlsxToGtfs.exe",icon='codigo/icono/icono.ico')])
